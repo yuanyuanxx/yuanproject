61ccc07b2b6b75d0c65874b526637d53ef3c084e
zhouyuan <zyj420@126.com>
2019-05-13 16:37:21 +0800

    update

--- API_Automation/Params/Params.py
+++
@@ -1,9 +0,0 @@
-# class Basic:
-#     params=get_parameter('Basic')
-#     url=[]
-#     #data=[]
-#     header=[]
-#     for i in range(0,len(params)):
-#         url.append(params[i]['url'])
-#         header.append(params[i]['header'])
-#
